fa687cceaa0e194d587ed680364f7fa0a08cea84
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-24 13:17:46 -0500

    the base file and images are showing on the page

--- church_root/pages/views.py
+++ church_root/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>IPC Church Homepage</h1>")
+    # return HttpResponse("<h1>IPC Church Homepage</h1>")
+    return render(request, 'base.html')
