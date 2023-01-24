2ec0cb177b4d7e268eb5083b07e4938a86e63e9f
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-23 22:44:51 -0500

    the views page works and now added a model

--- church_root/church_site/settings.py
+++ church_root/church_site/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth', 
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
--- church_root/pages/models.py
+++ church_root/pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=60)
+    permalink = models.CharField(max_length=12, unique=True)
+    update_date = models.DateTimeField('Recent Date')
+    bodytext = models.TextField('Page Info', blank=True)
