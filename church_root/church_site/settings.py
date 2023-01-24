3053296cfc1c5001e5ca6c3d8381e5ad36ccc56f
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-24 12:24:09 -0500

    add static and template files

--- church_root/church_site/settings.py
+++ church_root/church_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'church_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'church_site/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'church_site/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- church_root/pages/admin.py
+++ church_root/pages/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Page
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title','update')
+    list_display = ('title','update_date')
     ordering = ('title',)
     search_fields = ('title',)
 
