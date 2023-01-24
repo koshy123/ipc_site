c0a491303594178a34613fdb559871ce7c8fc212
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-24 16:33:53 -0500

    now the name bar is conncetd to various url/pages

--- church_root/pages/views.py
+++ church_root/pages/views.py
@@ -8,5 +8,6 @@ def index(request, pagename):
         'title': pg.title,
         'content': pg.bodytext,
         'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
     }
     return render(request, 'pages/page.html', context)
