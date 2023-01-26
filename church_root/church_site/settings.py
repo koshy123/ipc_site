2028b79c32bca8182c7298e17193b518a0fd0e53
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-25 21:26:38 -0500

    create new model

--- church_root/church_site/settings.py
+++ church_root/church_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'quotes.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth', 
     'django.contrib.contenttypes',
--- church_root/pages/views.py
+++ church_root/pages/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
 
 from . models import Page
-from . forms import ContactForm
+from .forms import ContactForm
 
 def index(request, pagename):
     pagename = '/' + pagename
@@ -23,6 +24,14 @@ def contact(request):
         if form.is_valid():
             cd = form.cleaned_data
             # assert False
+            con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com'],
+                connection=con,
+            )
             return HttpResponseRedirect('/contact?submitted=True')
     else:
         form = ContactForm()
