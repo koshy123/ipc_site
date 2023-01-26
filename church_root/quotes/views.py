85fd9b8d33434bfd3ab64fc3847c80e674d2e96a
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-26 13:10:26 -0500

    able to see full menu and prayer request menu

--- church_root/quotes/views.py
+++ church_root/quotes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Quote
 from .forms import QuoteForm
@@ -9,6 +10,21 @@ from pages.models import Page
 class QuoteList(ListView):
     model = Quote
     context_object_name = 'all_quotes'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteList, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
+
+class QuoteView(DetailView):
+    model = Quote
+    context_object_name = 'quote'
+    
+    def get_context_data(self, **kwargs):
+        context = super(QuoteView, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
+
 
 def quote_req(request):
     submitted = False
