fe50c2e5bb460a3d451bf366fe4c0f94386bb452
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-31 13:42:30 -0500

    download files to do heroku live

--- church_root/church_site/settings.py
+++ church_root/church_site/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-5sp&xyz18o-m+a-(a^3)_nr0nd7_vk^*wc71mt2#1%qpz1r9g&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'church_site/static']
 
 # Default primary key field type
